269f4441fdab65a41f55b775a438f88a3a8d7d33
你好呀 <431761794@qq.com>
2024-11-01 18:41:34 +0800

    1

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -56,23 +56,7 @@ const CONFIG = {
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      color_1: '#989bf8',
-      title_2: 'GPT',
-      img_2: '/images/heo/gpt.webp',
-      color_2: '#ffffff'
-    },
-    {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
-      title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
-      color_2: '#4082c3'
-    },
-    {
-      title_1: 'FinalCutPro',
+      title_1: 'Github',
       img_1: '/images/heo/github.webp',
       color_1: '#000000',
       title_2: 'Python',
@@ -88,25 +72,49 @@ const CONFIG = {
       color_2: '#8f55ba'
     },
     {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
-      color_1: '#f29e39',
+      title_1: 'GPT',
+      img_1: '/images/heo/gpt.webp',
+      color_1: '#ffffff',
       title_2: 'CSS3',
       img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
       color_2: '#2c51db'
     },
     {
-      title_1: 'JS',
-      img_1: '/images/heo/linux.webp',
-      color_1: '#F5BE04',
+      title_1: 'Typora',
+      img_1: '/images/heo/typora.webp',
+      color_1: '#e8ddcb',
       title_2: 'HTML',
       img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
       color_2: '#e9572b'
     },
     {
-      title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
-      color_1: '#df5b40',
+      title_1: 'matlab',
+      img_1: '/images/heo/matlab.webp',
+      color_1: '#edd3ff',
+      title_2: 'IDEA',
+      img_2: '/images/heo/idea.webp',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'AfterEffect',
+      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
+      color_1: '#989bf8',
+      title_2: 'illustrator',
+      img_2: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
+      color_2: '#f29e39'
+    },
+    {
+      title_1: 'Docker',
+      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
+      color_1: '#57b6e6',
+      title_2: 'Photoshop',
+      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
+      color_2: '#4082c3'
+    },
+    {
+      title_1: 'LINUX',
+      img_1: '/images/heo/linux.webp',
+      color_1: '#F5BE04',
       title_2: 'Rhino',
       img_2: '/images/heo/java.webp',
       color_2: '#E51837'
