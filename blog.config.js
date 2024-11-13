311e4b404cad8bf3cd1dd46b142d20eef0f26f48
你好呀 <111191349+88lin@users.noreply.github.com>
2024-11-13 18:00:17 +0800

    更新版本

--- blog.config.js
+++ blog.config.js
@@ -100,7 +100,7 @@ const BLOG = {
   ],
   FONT_AWESOME:
     process.env.NEXT_PUBLIC_FONT_AWESOME_PATH ||
-    'https://cdn.bootcdn.net/ajax/libs/font-awesome/6.4.2/css/all.css', // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
+    'https://cdn.bootcdn.net/ajax/libs/font-awesome/6.6.0/css/all.min.css', // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
 
   // END ************网站字体*****************
 
