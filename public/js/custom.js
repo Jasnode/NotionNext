d37f7d4de936e3e0b95281df51ab1b2df42f8d81
你好呀 <111191349+88lin@users.noreply.github.com>
2024-11-16 17:18:19 +0800

    1

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,14 @@
-// 这里编写自定义js脚本；将被静态引入到页面中
+// 自定义页面标题变化脚本
+var originalTitle = document.title;
+var timeout;
+document.addEventListener('visibilitychange', function () {
+    if (document.visibilityState === 'visible') {
+        document.title = '欢迎回来!💝' + originalTitle;
+        timeout = setTimeout(function () {
+            document.title = originalTitle;
+        }, 3000);
+    } else {
+        document.title = '你不要我了么w(ﾟДﾟ)w';
+        clearTimeout(timeout);
+    }
+});
