1f427ca0a8976ae8a110c85cb7709c824eac94ce
你好呀 <431761794@qq.com>
2025-02-08 22:56:12 +0800

    1

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -7,7 +7,7 @@ import { siteConfig } from '@/lib/config'
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>✨Powered by</span>
+      <span>✨Powered by</span>
       <a
         href='https://github.com/88lin/NotionNext'
         className='underline justify-start'>
