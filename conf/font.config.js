d11fa5027a5e1c91e614644520ce0a8ecac4de31
你好呀 <431761794@qq.com>
2025-03-20 21:40:55 +0800

    1

--- conf/font.config.js
+++ conf/font.config.js
@@ -6,10 +6,10 @@ module.exports = {
   // START ************网站字体*****************
   // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
   // 后面空格隔开的font-light的字体粗细，留空是默认粗细；参考 https://www.tailwindcss.cn/docs/font-weight
-  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-light',
+  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-semibold',
   // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
-    'https://cdn.jsdmirror.com/npm/lxgw-wenkai-gb-web@1.510.0/style.min.css',
+    'https://cdn.jsdmirror.com/npm/lxgw-wenkai-webfont@1.7.0/style.min.css',
     'https://fonts.googleapis.com/css?family=Bitter&display=swap', // Bitter 用作英文数字的字体
     'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
@@ -19,7 +19,7 @@ module.exports = {
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
-    '"LXGW WenKai GB"',
+    '"LXGW WenKai"',
     '"Microsoft YaHei"',
     '"Apple Color Emoji"',
     '"Segoe UI Emoji"',
@@ -35,7 +35,7 @@ module.exports = {
   ],
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
-    '"LXGW WenKai GB"',
+    '"LXGW WenKai"',
     'Bitter',
     '"Noto Serif SC"',
     'SimSun',
