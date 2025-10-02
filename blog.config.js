8fe083a6db3c989f5a73726b37c902f0e5446f53
你好呀 <431761794@qq.com>
2025-10-02 22:33:59 +0800

    1

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Spring', // 您的昵称
   BIO: process.env.NEXT_PUBLIC_BIO || '分享技术与生活的无限可能✨', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://pro.gleeze.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://nuaa.tech', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '茉灵智库,Spring,记录生活,分享资源,资源,文献,考研,技术教程,Windows,chatgpt,软件', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon1.svg', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '萌ICP备20249980号', // 备案号 闽ICP备XXXXXX
