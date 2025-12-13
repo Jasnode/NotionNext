8accc2185a32ee1237a461dcc0632d117ed903a4
你好呀 <431761794@qq.com>
2025-12-13 09:52:45 +0800

    站点域名

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Spring', // 您的昵称
   BIO: process.env.NEXT_PUBLIC_BIO || '分享技术与生活的无限可能✨', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://nuaa.tech', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://easys.cc.cd', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '论文下载, 电子书资源, 技术教程, Windows故障排查, 考研资料, 面试题库, 经验分享, 软件资源, 工具推荐', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon1.svg', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '萌ICP备20249980号', // 备案号 闽ICP备XXXXXX
