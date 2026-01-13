f2a7d35d06a70e7ff653de57b67959a607adb0e3
你好呀 <431761794@qq.com>
2026-01-13 09:14:23 +0800

    统计

--- components/AnalyticsBusuanzi.js
+++ components/AnalyticsBusuanzi.js
@@ -7,11 +7,17 @@ export default function AnalyticsBusuanzi() {
     <div className='flex gap-x-1'>
       <span className='hidden busuanzi_container_site_pv whitespace-nowrap'>
         <i className='fas fa-eye' />
-        <span className='px-1 busuanzi_site_pv'> </span>
+        <span
+          id='busuanzi_site_pv'
+          className='px-1 busuanzi_value_site_pv'
+        > </span>
       </span>
       <span className='hidden busuanzi_container_site_uv whitespace-nowrap'>
         <i className='fas fa-users' />
-        <span className='px-1 busuanzi_site_uv'> </span>
+        <span
+          id='busuanzi_site_uv'
+          className='px-1 busuanzi_value_site_uv'
+        > </span>
       </span>
     </div>
   )
