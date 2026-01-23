5fe49d4ddc60c52a27f33ea0db2bbb5c603003e8
你好呀 <431761794@qq.com>
2026-01-23 10:05:31 +0800

    修改配置

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
       {/* 颜色过度区 */}
       <div
         id='color-transition'
-        className='h-32 bg-gradient-to-b from-[#f1f2f3] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
+        className='h-20 bg-gradient-to-b from-[#f1f2f3] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
       />
 
       {/* 社交按钮 */}
